403b92e48e81da4f179a91f3d1d692df1b54ebc4
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-21 19:21:34 +0200

    feat(git): add config

--- home/git.nix
+++ home/git.nix
@@ -3,6 +3,32 @@
 {
   programs.git = {
     enable = true;
+
+    userEmail = "14175665+benjlevesque@users.noreply.github.com";
+    userName = "Benjamin Levesque";
+    signing = {
+      key = "0x7B58E6DDBA4ECC8D";
+    };
+    extraConfig = {
+      branch.sort = "-committerdate";
+      commit.gpgsign = true;
+      core.abrrev = 7;
+      "diff \"gpg\"".textconv = "gpg --no-tty --decrypt";
+      fetch.prune = true;
+      init.defaultBranch = "main";
+      log.date = "relative";
+      rebase.autoStash = true;
+      rerere.enabled = true;
+      tag.sort = "-v:refname";
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+      pull = {
+        ff = "only";
+        rebase = true;
+      };
+    };
   };
   home.packages = with pkgs; [
     gh
