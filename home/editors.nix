95f59fffd0bfc0aa5e6c705782d8d90948e3bbf1
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-23 21:18:17 +0200

    feat(vscode): settings + extensions

--- home/editors.nix
+++ home/editors.nix
@@ -11,7 +11,28 @@
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
       brettm12345.nixfmt-vscode
+      esbenp.prettier-vscode
+      dbaeumer.vscode-eslint
     ];
+
+    userSettings = {
+      "editor.fontFamily" = "FiraCode Nerd Font Mono";
+
+      "window.menuBarVisibility" = "toggle";
+      "nixfmt.path" = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+      "editor.formatOnSave" = true;
+      "scm.defaultViewMode" = "tree";
+      "terminal.integrated.defaultProfile.linux" = null;
+      "terminal.integrated.shell.linux" = {
+        bash = "/run/current-system/sw/bin/bash";
+      };
+      "[typescriptreact]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[typescript]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+    };
   };
 
   programs.neovim = {
