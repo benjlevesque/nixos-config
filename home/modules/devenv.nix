045893878a850417c9df166e525ede65f9a22010
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-11-20 11:55:56 +0100

    feat(direnv): reduce verbosity

--- home/modules/devenv.nix
+++ home/modules/devenv.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
 
-  programs.direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+    config = {
+      global.hide_env_diff = true;
+    };
+  };
   home.packages = with pkgs; [
     devenv
   ];
