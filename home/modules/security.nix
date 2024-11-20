2aa5f0ef3f8a3690dbc15775d4e11debc100530e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-11-20 11:54:21 +0100

    feat(gpg): increase ttl

--- home/modules/security.nix
+++ home/modules/security.nix
@@ -9,6 +9,8 @@
   services.gpg-agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-gnome3;
+    maxCacheTtl = 4 * 60 * 60; # 4 hours
+    defaultCacheTtl = 1 * 60 * 60; # 1 hour
   };
 
   programs.gpg.enable = true;
