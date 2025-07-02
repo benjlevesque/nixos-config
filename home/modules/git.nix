ab8a9d07e52804e8e2ac0707115dac30c23a2514
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-07-02 19:20:02 +0200

    feat: git config

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,5 +1,19 @@
 { pkgs, ... }:
-
+let
+  gitShellAliases = {
+    gco = "git checkout";
+    gcb = "git checkout -b";
+    gst = "git status";
+    grb = "git rebase";
+    grbc = "git rebase --continue";
+    gp = "git push";
+    gpf = "git push --force-with-lease";
+    gcm = "git commit --message";
+    glog = "git log --oneline --decorate --graph";
+    pr = "${pkgs.gh}/bin/gh pr create --fill-first";
+    ghst = "${pkgs.gh}/bin/gh status";
+  };
+in
 {
   programs.git = {
     enable = true;
@@ -14,21 +28,46 @@
       commit.gpgsign = true;
       core.abrrev = 7;
       "diff \"gpg\"".textconv = "gpg --no-tty --decrypt";
-      fetch.prune = true;
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      column.ui = "auto";
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
       init.defaultBranch = "main";
       log.date = "relative";
       rebase.autoStash = true;
       rerere.enabled = true;
       tag.sort = "-v:refname";
+      # tag.sort = "version:refname";
       push = {
         default = "current";
+        #  default = simple
         autoSetupRemote = true;
+        followTags = true;
       };
       pull = {
         ff = "only";
         rebase = true;
       };
     };
+    ignores = [
+      ".vscode"
+
+      "devenv.nix"
+      "devenv.lock"
+      "devenv.yaml"
+      ".devenv.flake.nix"
+      ".devenv/"
+      ".direnv/"
+      ".envrc"
+    ];
   };
 
   programs.gh = {
@@ -42,19 +81,9 @@
 
   };
 
-  programs.bash.shellAliases = {
-    gco = "git checkout";
-    gcb = "git checkout -b";
-    gst = "git status";
-    grb = "git rebase";
-    grbc = "git rebase --continue";
-    gp = "git push";
-    gpf = "git push --force-with-lease";
-    gcm = "git commit --message";
-    glog = "git log --oneline --decorate --graph";
-    pr = "git push && ${pkgs.gh}/bin/gh pr create --fill-first";
-    ghst = "${pkgs.gh}/bin/gh status";
-  };
+  programs.bash.shellAliases = gitShellAliases;
+  programs.nushell.shellAliases = gitShellAliases;
+
   programs.bash.initExtra = ''
     source ${pkgs.git}/share/bash-completion/completions/git
     _git_checkout_no_tags() {
@@ -62,5 +91,6 @@
     }
     __git_complete gco _git_checkout_no_tags
   '';
+
 
 }
